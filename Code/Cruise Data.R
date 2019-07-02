db35017046f92ac41c003296b1939a5f443b8d3b
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-07-02 15:25:55 +0100

    Tidying data

--- Code/Cruise Data.R
+++ Code/Cruise Data.R
@@ -1,4 +1,4 @@
-#### Cruise Data ####
+#### Data Arranging ####
 
 library(tidyverse)
 
@@ -26,3 +26,21 @@ myct_tidy <- select(myct,
 myct_comp <- left_join(myct_tidy, stn_comp, by = "Station")
 
 write.csv(myct_comp, "Data/Study_Myctophids_Full.csv")
+
+### Join to isotope data ###
+
+iso <- read.csv("Data/Otolith_Isotopes.csv")
+myct <- read.csv("Data/Study_Myctophids_Full.csv")
+
+myct_iso <- left_join(myct, iso, by = "MyNumber")
+
+write.csv(myct_iso, "Data/Myctophids_Master.csv")
+
+### Join to ecology data ###
+
+myct <- read.csv("Data/Myctophids_Master.csv")
+ecol <- read.csv("Data/Extra/Ecol_Table.csv")
+
+myct_ecol <- left_join(myct, ecol, by = "sciname")
+
+write.csv(myct_ecol, "Data/Myctophids_Master.csv")
--- Code/Summary.R
+++ Code/Summary.R
@@ -6,4 +6,14 @@ library(readr)
 Myctophids <- read_csv("Data/Myctophids.csv")
 Study_Myctophids  <- filter(Myctophids, Otolith != "NA")
 Count_Myctophids <- count(Study_Myctophids, sciname)
-KRA <- filter(Study_Myctophids, Label == "KRA")
+
+write.csv(Study_Myctophids, "Data/Study_Myctophids.csv", row.names = F)
+
+# Need metadata
+
+Metadata_Myctophids <- filter(Study_Myctophids, is.na(Weight))
+Metadata_Myctophids <- select(Metadata_Myctophids, ID, sciname, Label, Station, Notes)
+
+# Write into CSV file
+
+write.csv(Metadata_Myctophids, "Data/Myctophids_wo_Metadata.csv", row.names = F)
