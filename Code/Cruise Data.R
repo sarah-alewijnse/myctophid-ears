100bf8ec7fb5a4be4dc1f2c0dea3b38c88fae9eb
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-07-16 16:23:39 +0100

    Changed phyto to be based on Longhurst province

--- Code/Cruise Data.R
+++ Code/Cruise Data.R
@@ -2,7 +2,7 @@
 
 library(tidyverse)
 
-myct <- read.csv("Data/Study_Myctophids.csv")
+myct <- read.csv("Data/Extra/Study_Myctophids.csv")
 
 ### Get cruise data ###
 
@@ -10,31 +10,18 @@ stn_comp <- read.csv("Data/Metadata/Station_Data.csv")
 
 ### Join to data ####
 
-myct_tidy <- select(myct,
-                    MyNumber,
-                    ID,
-                    sciname,
-                    Label,
-                    Station,
-                    Notes,
-                    Muscle,
-                    Otolith,
-                    Weight,
-                    Weight_SD,
-                    Sex)
+myct_comp <- left_join(myct, stn_comp, by = "Station")
 
-myct_comp <- left_join(myct_tidy, stn_comp, by = "Station")
-
-write.csv(myct_comp, "Data/Study_Myctophids_Full.csv")
+write.csv(myct_comp, "Data/Extra/Study_Myctophids_Full.csv", row.names = F)
 
 ### Join to isotope data ###
 
-iso <- read.csv("Data/Otolith_Isotopes.csv")
-myct <- read.csv("Data/Study_Myctophids_Full.csv")
+iso <- read.csv("Data/Extra/Otolith_Isotopes.csv")
+myct <- read.csv("Data/Extra/Study_Myctophids_Full.csv")
 
 myct_iso <- left_join(myct, iso, by = "MyNumber")
 
-write.csv(myct_iso, "Data/Myctophids_Master.csv")
+write.csv(myct_iso, "Data/Myctophids_Master.csv", row.names = F)
 
 ### Join to ecology data ###
 
@@ -43,4 +30,4 @@ ecol <- read.csv("Data/Extra/Ecol_Table.csv")
 
 myct_ecol <- left_join(myct, ecol, by = "sciname")
 
-write.csv(myct_ecol, "Data/Myctophids_Master.csv")
+write.csv(myct_ecol, "Data/Myctophids_Master.csv", row.names = F)
