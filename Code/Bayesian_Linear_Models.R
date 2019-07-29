8a32d7109adb4d35f01691d451f4d6d686985a9e
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-07-29 18:19:21 +0100

    Started single species models

--- Code/Bayesian_Linear_Models.R
+++ Code/Bayesian_Linear_Models.R
@@ -6,7 +6,7 @@ library(rethinking)
 myct <- read.csv("Outputs/Combined.csv")
 glimpse(myct)
 
-#### Model with Weight ####
+#### Overall Model with Weight ####
 
 myct_tidy <- filter(myct, Weight_SD == "0")
 myct_tidy$log10_Weight <- log10(myct_tidy$Weight.x)
@@ -23,17 +23,17 @@ model_M_W <- map2stan(
     M_est ~ dnorm(mu, sigma),
     mu <- a + b*Weight,
     M_obs ~ dnorm(M_est, M_sd),
-    a ~ dnorm(0.2, 1),
+    a ~ dnorm(0.25, 1),
     b ~ dnorm(0, 1),
-    sigma ~ dunif(0, 10)
+    sigma ~ dunif(0, 1)
   ),
 data = M_W_list,
 start = list(M_est = M_W_list$M_obs),
 WAIC = FALSE,
 iter = 5000,
 warmup = 1000,
-chains = 2,
-cores = 2,
+chains = 1,
+cores = 1,
 control = list(adapt_delta = 0.95))
 
 ## Precis Tables
@@ -59,5 +59,59 @@ mu.HPDI <- apply(mu, 2, HPDI, prob = 0.95)
 
 par(mfrow = c(1,1))
 plot(M_obs ~ Weight, data = M_W_list, pch = 16, col = Species, cex = 1.5)
+lines(weight_seq, mu.mean)
+shade(mu.HPDI, weight_seq)
+
+#### Model with Species ####
+
+PRM <- filter(myct_tidy, Label == "PRM")
+
+PRM_M_W_list <- list(
+  M_obs = PRM$M,
+  M_sd = PRM$sd_M,
+  Weight = PRM$log10_Weight
+)
+
+PRM_M_W <- map2stan(
+  alist(
+    M_est ~ dnorm(mu, sigma),
+    mu <- a + b*Weight,
+    M_obs ~ dnorm(M_est, M_sd),
+    a ~ dnorm(0.25, 1),
+    b ~ dnorm(0, 1),
+    sigma ~ dunif(0, 1)
+  ),
+  data = PRM_M_W_list,
+  start = list(M_est = PRM_M_W_list$M_obs),
+  WAIC = FALSE,
+  iter = 5000,
+  warmup = 1000,
+  chains = 1,
+  cores = 1,
+  control = list(adapt_delta = 0.95))
+
+## Precis Tables
+
+precis(PRM_M_W)
+precis(PRM_M_W, depth = 2)
+
+## Check chains
+
+plot(PRM_M_W)
+
+## Plot
+
+weight_seq <- seq(from = min(PRM_M_W_list$Weight), to = max(PRM_M_W_list$Weight), by = 0.01) # Horizontal axis
+mu <- link(PRM_M_W, data = data.frame(Weight = weight_seq)) # Link model to mu
+
+# Summarise the distribution of mu
+
+mu.mean <- apply(mu, 2, mean)
+mu.HPDI <- apply(mu, 2, HPDI, prob = 0.95)
+
+# Plot with model and raw data
+
+par(mfrow = c(1,1))
+plot(M_obs ~ Weight, data = PRM_M_W_list, pch = 16, cex = 1.5)
 lines(weight_seq, mu.mean)
 shade(mu.HPDI, weight_seq)
