7922177a1acaaabae6b0374bc8718f69a77651ac
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-07-31 15:24:40 +0100

    Made plot pretty

--- Code/Density_Plot.R
+++ Code/Density_Plot.R
@@ -54,7 +54,7 @@ GYR$sciname <- "Gymnoscopelus braueri"
 
 GYN <- M_values[560001:680000,]
 colnames(GYN) <- c("M", "sciname")
-GYN$sciname <- "Gymnoscopelus nichosi"
+GYN$sciname <- "Gymnoscopelus nicholsi"
 
 KRA <- M_values[680001:880000,]
 colnames(KRA) <- c("M", "sciname")
@@ -68,11 +68,52 @@ PRM$sciname <- "Protomyctophum bolini"
 
 add <- rbind(ELN, ELC, GYR, GYN, KRA, PRM)
 
+## Get densities
+
+max <- which.max(density(ELN$M)$y)
+ELN_dens <- density(ELN$M)$x[max]
+
+max <- which.max(density(ELC$M)$y)
+ELC_dens <- density(ELC$M)$x[max]
+
+max <- which.max(density(GYR$M)$y)
+GYR_dens <- density(GYR$M)$x[max]
+
+max <- which.max(density(GYN$M)$y)
+GYN_dens <- density(GYN$M)$x[max]
+
+max <- which.max(density(KRA$M)$y)
+KRA_dens <- density(KRA$M)$x[max]
+
+max <- which.max(density(PRM$M)$y)
+PRM_dens <- density(PRM$M)$x[max]
+
+max_dens <- data.frame(M = c(ELN_dens, ELC_dens, GYR_dens, GYN_dens, KRA_dens, PRM_dens),
+                       sciname = c("Electrona antarctica", "Electrona carlsbergi", "Gymnoscopelus braueri", "Gymnoscopelus nicholsi", "Krefftichthys anderssoni", "Protomyctophum bolini"))
+
+
 ## Attempt plot
 
-ggplot(add, aes(M))+
-  geom_density()+
-  facet_grid(. ~sciname)
+cbp1 <- c("#56B4E9", "#0072B2", "#E69F00", "#D55E00", "#009E73", "#CC79A7")
 
-ggplot(add, aes(x = M, colour = sciname))+
-  geom_density()
+ggplot(add, aes(x = M))+
+  geom_density()+
+  facet_grid(.~sciname)
+
+ggplot(add, aes(x = M, fill = sciname))+
+  geom_density(alpha = 0.5, lwd = 1, colour = NA)+
+  geom_vline(data = max_dens, aes(xintercept = M, colour = sciname), lwd = 2)+
+  scale_fill_manual(name = "Species", values = cbp1) +
+  scale_colour_manual(name = "Species", values = cbp1) +
+  scale_x_continuous(breaks = round(seq(min(add$M), max(add$M), by = 0.05),1), limits = c(0.1, 0.5)) +
+  scale_y_continuous(limits = c(0, 25), expand = c(0, 0))+
+  ylab("Density")+
+  theme(panel.background = element_blank(), # Keep the background blank
+        text = element_text(size = 15),
+        panel.border = element_rect(colour = "black", fill = NA),
+        legend.title = element_text(),
+        legend.key=element_blank(),
+        legend.text = element_text(face = "italic"),
+        axis.text.x = element_text(colour = "black"),
+        axis.text.y = element_text(colour = "black"))
+
