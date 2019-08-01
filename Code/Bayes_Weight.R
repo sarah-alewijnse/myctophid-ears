5300fbda983725d34e9c3bce2797ed0d72790f24
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-08-01 11:44:17 +0100

    Reran weight analysis with ln(Weight)

--- Code/Bayes_Weight.R
+++ Code/Bayes_Weight.R
@@ -9,12 +9,12 @@ glimpse(myct)
 #### Overall Model with Weight ####
 
 myct_tidy <- filter(myct, Weight_SD == "0")
-myct_tidy$log10_Weight <- log10(myct_tidy$Weight.x)
+myct_tidy$ln_Weight <- log(myct_tidy$Weight.x)
 
 M_W_list <- list(
   M_obs = myct_tidy$M,
   M_sd = myct_tidy$sd_M,
-  Weight = myct_tidy$log10_Weight,
+  Weight = myct_tidy$ln_Weight,
   Species = myct_tidy$sciname
 )
 
@@ -73,7 +73,7 @@ ELN <- filter(myct_tidy, Label == "ELN")
 ELN_M_W_list <- list(
   M_obs = ELN$M,
   M_sd = ELN$sd_M,
-  Weight = ELN$log10_Weight
+  Weight = ELN$ln_Weight
 )
 
 ELN_M_W <- map2stan(
@@ -117,7 +117,7 @@ mu.HPDI <- apply(mu, 2, HPDI, prob = 0.95)
 par(mfrow = c(1,1))
 par(mar=c(4,4,3,3))
 plot(M_obs ~ Weight, data = ELN_M_W_list, pch = 16, cex = 1.5, ylim = c(0.17, 0.36), xlab = "", ylab = "", tck = -0.01)
-mtext(text = "log10(Weight) (g)", side = 1, line = 2)
+mtext(text = "Ln(Weight) (g)", side = 1, line = 2)
 mtext(text = "M Value", side = 2, line = 2)
 with(ELN_M_W_list, arrows(Weight, M_obs - M_sd, Weight, M_obs + M_sd, length = 0.05, angle = 90 , code = 3))
 lines(weight_seq, mu.mean, lwd = 3)
@@ -130,7 +130,7 @@ ELC <- filter(myct_tidy, Label == "ELC")
 ELC_M_W_list <- list(
   M_obs = ELC$M,
   M_sd = ELC$sd_M,
-  Weight = ELC$log10_Weight
+  Weight = ELC$ln_Weight
 )
 
 ELC_M_W <- map2stan(
@@ -163,8 +163,8 @@ plot(ELC_M_W)
 
 par(mfrow = c(1,1))
 par(mar=c(4,4,3,3))
-plot(M_obs ~ Weight, data = ELC_M_W_list, pch = 16, cex = 1.5, xlab = "", ylab = "", xlim = c(0.65, 0.95), ylim = c(0.15, 0.25), tck = -0.01)
-mtext(text = "log10(Weight) (g)", side = 1, line = 2)
+plot(M_obs ~ Weight, data = ELC_M_W_list, pch = 16, cex = 1.5, xlab = "", ylab = "", ylim = c(0.15, 0.25), tck = -0.01)
+mtext(text = "Ln(Weight) (g)", side = 1, line = 2)
 mtext(text = "M Value", side = 2, line = 2)
 with(ELC_M_W_list, arrows(Weight, M_obs - M_sd, Weight, M_obs + M_sd, length = 0.05, angle = 90 , code = 3))
 
@@ -175,7 +175,7 @@ GYR <- filter(myct_tidy, Label == "GYR")
 GYR_M_W_list <- list(
   M_obs = GYR$M,
   M_sd = GYR$sd_M,
-  Weight = GYR$log10_Weight
+  Weight = GYR$ln_Weight
 )
 
 GYR_M_W <- map2stan(
@@ -219,7 +219,7 @@ mu.HPDI <- apply(mu, 2, HPDI, prob = 0.95)
 par(mfrow = c(1,1))
 par(mar=c(4,4,3,3))
 plot(M_obs ~ Weight, data = GYR_M_W_list, pch = 16, cex = 1.5, xlab = "", ylab = "", ylim = c(0.17, 0.35), tck = -0.01)
-mtext(text = "log10(Weight) (g)", side = 1, line = 2)
+mtext(text = "ln(Weight) (g)", side = 1, line = 2)
 mtext(text = "M Value", side = 2, line = 2)
 with(GYR_M_W_list, arrows(Weight, M_obs - M_sd, Weight, M_obs + M_sd, length = 0.05, angle = 90 , code = 3))
 lines(weight_seq, mu.mean, lwd = 3)
@@ -232,7 +232,7 @@ GYN <- filter(myct_tidy, Label == "GYN")
 GYN_M_W_list <- list(
   M_obs = GYN$M,
   M_sd = GYN$sd_M,
-  Weight = GYN$log10_Weight
+  Weight = GYN$ln_Weight
 )
 
 GYN_M_W <- map2stan(
@@ -276,7 +276,7 @@ mu.HPDI <- apply(mu, 2, HPDI, prob = 0.95)
 par(mfrow = c(1,1))
 par(mar=c(4,4,3,3))
 plot(M_obs ~ Weight, data = GYN_M_W_list, pch = 16, cex = 1.5, xlab = "", ylab = "", ylim = c(0.12, 0.25), tck = -0.01)
-mtext(text = "log10(Weight) (g)", side = 1, line = 2)
+mtext(text = "ln(Weight) (g)", side = 1, line = 2)
 mtext(text = "M Value", side = 2, line = 2)
 with(GYN_M_W_list, arrows(Weight, M_obs - M_sd, Weight, M_obs + M_sd, length = 0.05, angle = 90 , code = 3))
 lines(weight_seq, mu.mean, lwd = 3)
@@ -289,7 +289,7 @@ KRA <- filter(myct_tidy, Label == "KRA")
 KRA_M_W_list <- list(
   M_obs = KRA$M,
   M_sd = KRA$sd_M,
-  Weight = KRA$log10_Weight
+  Weight = KRA$ln_Weight
 )
 
 KRA_M_W <- map2stan(
@@ -320,12 +320,27 @@ plot(KRA_M_W)
 
 # Plot with model and raw data
 
+## Plot
+
+weight_seq <- seq(from = min(KRA_M_W_list$Weight), to = max(KRA_M_W_list$Weight), by = 0.001) # Horizontal axis
+mu <- link(KRA_M_W, data = data.frame(Weight = weight_seq)) # Link model to mu
+
+# Summarise the distribution of mu
+
+mu.mean <- apply(mu, 2, mean)
+mu.HPDI <- apply(mu, 2, HPDI, prob = 0.95)
+
+# Plot with model and raw data
+
 par(mfrow = c(1,1))
 par(mar=c(4,4,3,3))
-plot(M_obs ~ Weight, data = KRA_M_W_list, pch = 16, cex = 1.5, xlab = "", ylab = "", ylim = c(0.18, 0.35), tck = -0.01)
-mtext(text = "log10(Weight) (g)", side = 1, line = 2)
+plot(M_obs ~ Weight, data = KRA_M_W_list, pch = 16, cex = 1.5, xlab = "", ylab = "", ylim = c(0.17, 0.35), tck = -0.01)
+mtext(text = "ln(Weight) (g)", side = 1, line = 2)
 mtext(text = "M Value", side = 2, line = 2)
 with(KRA_M_W_list, arrows(Weight, M_obs - M_sd, Weight, M_obs + M_sd, length = 0.05, angle = 90 , code = 3))
+lines(weight_seq, mu.mean, lwd = 3)
+shade(mu.HPDI, weight_seq)
+
 
 ## PRM
 
@@ -334,7 +349,7 @@ PRM <- filter(myct_tidy, Label == "PRM")
 PRM_M_W_list <- list(
   M_obs = PRM$M,
   M_sd = PRM$sd_M,
-  Weight = PRM$log10_Weight
+  Weight = PRM$ln_Weight
 )
 
 PRM_M_W <- map2stan(
@@ -378,7 +393,7 @@ mu.HPDI <- apply(mu, 2, HPDI, prob = 0.95)
 par(mfrow = c(1,1))
 par(mar=c(4,4,3,3))
 plot(M_obs ~ Weight, data = PRM_M_W_list, pch = 16, cex = 1.5, xlab = "", ylab = "", ylim = c(0.13, 0.3), tck = -0.01)
-mtext(text = "log10(Weight) (g)", side = 1, line = 2)
+mtext(text = "ln(Weight) (g)", side = 1, line = 2)
 mtext(text = "M Value", side = 2, line = 2)
 with(PRM_M_W_list, arrows(Weight, M_obs - M_sd, Weight, M_obs + M_sd, length = 0.05, angle = 90 , code = 3))
 lines(weight_seq, mu.mean, lwd = 3)
