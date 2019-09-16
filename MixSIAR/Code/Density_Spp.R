9c98b0891fe684c58995d3250135e9f98dc3e88f
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-09-16 17:02:25 +0100

    Got density plot with regular mean

--- MixSIAR/Code/Density_Spp.R
+++ MixSIAR/Code/Density_Spp.R
@@ -12,6 +12,12 @@ spp <- select(myct, MyNumber, sciname)
 
 post_M_spp <- left_join(post_M, spp, by = "MyNumber")
 
+# Get means
+
+means <- aggregate(post_M_spp, by = list(post_M_spp$sciname), FUN = mean)
+colnames(means) <- c("sciname", "M", "MyNumber", "ds")
+means <- select(means, sciname, M)
+
 ## Plot
 
 cbp1 <- c("#56B4E9", "#0072B2", "#E69F00", "#D55E00", "#009E73", "deeppink")
@@ -21,10 +27,10 @@ ggplot(post_M_spp, aes(x = M))+
   facet_grid(.~sciname)
 
 ggplot(post_M_spp, aes(x = M, fill = sciname))+
-  geom_density(alpha = 0.5, lwd = 1, colour = NA)+
+  geom_density(alpha = 0.5, lwd = 1, colour = "black")+
+  geom_vline(data = means, aes(xintercept = M, colour = sciname), lwd = 2)+
   scale_fill_manual(name = "Species", values = cbp1) +
   scale_colour_manual(name = "Species", values = cbp1) +
-  #scale_x_continuous(breaks = round(seq(min(post_M_spp$M), max(post_M_spp$M), by = 0.05),1), limits = c(0.1, 0.5)) +
   scale_y_continuous(limits = c(0, 27), expand = c(0, 0))+
   ylab("Kernel Density")+
   xlab("M Values")+
