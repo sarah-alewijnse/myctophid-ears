b3245cd9932340ad029c49f1b649d30bf426b23b
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-09-16 16:27:58 +0100

    Combined posteriors of M values

---
+++ MixSIAR/Code/Combined_M_Post.R
@@ -0,0 +1,27 @@
+#### Combine Posteriors ####
+
+setwd("~/PhD/GitHub/mytophid-ears/MixSIAR/Outputs/Posteriors")
+
+library(tidyverse)
+library(MixSIAR)
+
+read_plus <- function(flnm) {
+  read_csv(flnm) %>% 
+    mutate(filename = flnm)
+}
+
+tbl_with_sources <-
+  list.files(pattern = "*.csv", 
+             full.names = T) %>% 
+  map_df(~read_plus(.))
+
+names <- tbl_with_sources$filename
+names <- substring(names, 11, 17)
+
+tbl_with_sources$filename <- names
+
+post <- tbl_with_sources
+post <- select(post, post_M, filename)
+colnames(post) <- c("M", "MyNumber")
+
+write.csv(post, "M_Post.csv")
