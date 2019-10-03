2003a64b2678523cce2cb38aa2825ac14583b8ff
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-10-03 14:50:07 +0100

    Finished coding density plots for M and temp

---
+++ MixSIAR/Code/Combined_Posteriors.R
@@ -0,0 +1,58 @@
+#### Combine Posteriors ####
+
+## M
+
+setwd("~/PhD/GitHub/mytophid-ears/MixSIAR/Outputs/M/Posteriors")
+
+library(tidyverse)
+library(MixSIAR)
+
+read_plus <- function(flnm) {
+  read_csv(flnm) %>% 
+    mutate(filename = flnm)
+}
+
+tbl_with_sources <-
+  list.files(pattern = "*.csv", 
+             full.names = T) %>% 
+  map_df(~read_plus(.))
+
+names <- tbl_with_sources$filename
+names <- substring(names, 11, 17)
+names <- gsub (" ", "", names, fixed = TRUE)
+
+tbl_with_sources$filename <- names
+
+post <- tbl_with_sources
+post <- select(post, post_M, filename)
+colnames(post) <- c("M", "MyNumber")
+
+write.csv(post, "M_Post.csv", row.names = FALSE)
+
+
+## Temperature
+
+setwd("~/PhD/GitHub/mytophid-ears/MixSIAR/Outputs/Temperature/Posteriors")
+
+read_plus <- function(flnm) {
+  read_csv(flnm) %>% 
+    mutate(filename = flnm)
+}
+
+tbl_with_sources <-
+  list.files(pattern = "*.csv", 
+             full.names = T) %>% 
+  map_df(~read_plus(.))
+
+names <- tbl_with_sources$filename
+names <- substring(names, 13, 19)
+names <- gsub(" ", "", names, fixed = TRUE)
+names <- gsub(".", "", names, fixed = TRUE)
+
+tbl_with_sources$filename <- names
+
+post <- tbl_with_sources
+post <- select(post, Temp, filename)
+colnames(post) <- c("Temp", "MyNumber")
+
+write.csv(post, "Temp_Post.csv", row.names = FALSE)
--- MixSIAR/Code/Density_Spp.R
+++ MixSIAR/Code/Density_Spp.R
@@ -1,7 +1,6 @@
-#### Species Density Plot ####
+#### Species Density Plot - M ####
 
 library(tidyverse)
-library(MixSIAR)
 
 post_M <- read.csv("MixSIAR/Outputs/M/Posteriors/M_Post.csv")
 
@@ -18,27 +17,83 @@ means <- aggregate(post_M_spp, by = list(post_M_spp$sciname), FUN = mean)
 colnames(means) <- c("sciname", "M", "MyNumber", "ds")
 means <- select(means, sciname, M)
 
+# Make sciname a factor
+
+post_M_spp$sciname <- factor(post_M_spp$sciname, levels = 
+                               c("Gymnoscopelus nicholsi",
+                                 "Protomyctophum bolini",
+                                 "Electrona carlsbergi",
+                                 "Krefftichthys anderssoni",
+                                 "Gymnoscopelus braueri",
+                                 "Electrona antarctica"))
+
 ## Plot
 
-cbp1 <- c("#56B4E9", "#0072B2", "#E69F00", "#D55E00", "#009E73", "deeppink")
-
-ggplot(post_M_spp, aes(x = M))+
-  geom_density()+
-  facet_grid(.~sciname)
+cbp1 <- c("#D55E00", "#CC79A7", "#56B4E9", "#009E73", "#E69F00", "#0072B2")
 
 ggplot(post_M_spp, aes(x = M, fill = sciname))+
-  geom_density(alpha = 0.5, lwd = 1, colour = "black")+
-  geom_vline(data = means, aes(xintercept = M, colour = sciname), lwd = 2)+
+  geom_density()+
+  facet_wrap(~sciname, ncol = 1) +
   scale_fill_manual(name = "Species", values = cbp1) +
   scale_colour_manual(name = "Species", values = cbp1) +
   scale_y_continuous(limits = c(0, 27), expand = c(0, 0))+
   ylab("Kernel Density")+
   xlab("M Values")+
+  geom_vline(data = means, aes(xintercept = M), lwd = 1)+
   theme(panel.background = element_blank(),
-        text = element_text(size = 25),
+        legend.position = "none",
+        strip.background = element_rect(fill = "white"),
+        strip.text.x = element_text(size = 10, face = "italic"),
+        text = element_text(size = 10),
         panel.border = element_rect(colour = "black", fill = NA),
-        legend.title = element_text(),
-        legend.key=element_blank(),
-        legend.text = element_text(face = "italic"),
+        axis.text.x = element_text(colour = "black"),
+        axis.text.y = element_text(colour = "black"))
+
+#### Species Density Plot - Temperature ####
+
+post_Temp <- read.csv("MixSIAR/Outputs/Temperature/Posteriors/Temp_Post.csv")
+
+## Join with species
+
+myct <- read.csv("Data/Myctophids_Master.csv")
+spp <- select(myct, MyNumber, sciname)
+
+post_Temp_spp <- left_join(post_Temp, spp, by = "MyNumber")
+
+# Get means
+
+means <- aggregate(post_Temp_spp, by = list(post_Temp_spp$sciname), FUN = mean)
+colnames(means) <- c("sciname", "Temp", "MyNumber", "ds")
+means <- select(means, sciname, Temp)
+
+# Make sciname a factor
+
+post_Temp_spp$sciname <- factor(post_Temp_spp$sciname, levels = 
+                               c("Gymnoscopelus braueri",
+                                 "Electrona antarctica",
+                                 "Krefftichthys anderssoni",
+                                 "Protomyctophum bolini",
+                                 "Gymnoscopelus nicholsi",
+                                 "Electrona carlsbergi"))
+
+## Plot
+
+cbp1 <- c("#E69F00", "#0072B2", "#009E73", "#CC79A7", "#D55E00", "#56B4E9")
+
+ggplot(post_Temp_spp, aes(x = Temp, fill = sciname))+
+  geom_density() +
+  facet_wrap(~sciname, ncol = 1) +
+  scale_fill_manual(name = "Species", values = cbp1) +
+  scale_colour_manual(name = "Species", values = cbp1) +
+  scale_y_continuous(limits = c(0, 0.27), expand = c(0, 0))+
+  ylab("Kernel Density")+
+  xlab("Temperature")+
+  geom_vline(data = means, aes(xintercept = Temp), lwd = 1)+
+  theme(panel.background = element_blank(),
+        legend.position = "none",
+        strip.background = element_rect(fill = "white"),
+        strip.text.x = element_text(size = 10, face = "italic"),
+        text = element_text(size = 10),
+        panel.border = element_rect(colour = "black", fill = NA),
         axis.text.x = element_text(colour = "black"),
         axis.text.y = element_text(colour = "black"))
