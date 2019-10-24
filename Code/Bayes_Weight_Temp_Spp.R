bcf5a6862e156d19929a6a4cd4da9f293dc7dce3
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-10-24 14:35:20 +0100

    Tidied repo

---
+++ Code/Bayes_Weight_Temp_Spp.R
@@ -0,0 +1,153 @@
+#### Bayesian Linear Models - Weight and Temp ####
+
+library(tidyverse)
+library(rethinking)
+library(bayesplot)
+
+options(max.print=999999)
+
+myct <- read.csv("Myctophids_M_Temp.csv")
+glimpse(myct)
+
+#### Overall Model with Weight and Temp ####
+
+myct_tidy <- filter(myct, Weight_SD == "0")
+myct_tidy <- filter(myct, !is.na(mean_M))
+myct_tidy$log_Weight <- log(myct_tidy$Weight.x)
+glimpse(myct_tidy)
+
+M_T_W_list <- list(
+  M_obs = myct_tidy$mean_M,
+  M_sd = myct_tidy$sd_M,
+  Weight = myct_tidy$log_Weight,
+  Temp_obs = myct_tidy$mean_Temp,
+  Temp_sd = myct_tidy$sd_Temp,
+  Species = myct_tidy$sciname
+)
+
+## Convert to z-scores
+
+# Weight
+
+Weight_mean <- mean(M_T_W_list$Weight)
+Weight_sd <- sd(M_T_W_list$Weight)
+
+for(i in 1:length(M_T_W_list$Weight)){
+  M_T_W_list$Weight_Z[i] <- (M_T_W_list$Weight[i] - Weight_mean) / Weight_sd
+}
+
+# Temp_obs
+
+Temp_obs_mean <- mean(M_T_W_list$Temp_obs)
+Temp_obs_sd <- sd(M_T_W_list$Temp_obs)
+
+for(i in 1:length(M_T_W_list$Temp_obs)){
+  M_T_W_list$Temp_Obs_Z[i] <- (M_T_W_list$Temp_obs[i] - Temp_obs_mean) / Temp_obs_sd
+}
+
+# Temp_sd
+
+Temp_sd_mean <- mean(M_T_W_list$Temp_sd)
+Temp_sd_sd <- sd(M_T_W_list$Temp_sd)
+
+for(i in 1:length(M_T_W_list$Temp_sd)){
+  M_T_W_list$Temp_SD_Z[i] <- abs((M_T_W_list$Temp_sd[i] - Temp_sd_mean) / Temp_sd_sd)
+}
+
+# Tidy list
+
+mod_list <- list(
+  M_obs = M_T_W_list$M_obs,
+  M_sd = M_T_W_list$M_sd,
+  Weight = M_T_W_list$Weight_Z,
+  Temp_obs = M_T_W_list$Temp_Obs_Z,
+  Temp_sd = M_T_W_list$Temp_SD_Z,
+  Species = M_T_W_list$Species
+)
+
+## Model
+
+model_M_T_W <- map2stan(
+  alist(
+    M_est ~ dnorm(mu , sigma),
+    
+    # Linear model
+    mu <- a + a_Var[Species] +
+      b_W*Weight +
+      b_T*Temp_est[i],
+    
+    # Data uncertainties
+    M_obs ~ dnorm(M_est, M_sd),
+    Temp_obs ~ dnorm(Temp_est, Temp_sd),
+    
+    # Parameters
+    a ~ dnorm(0, 1),
+    b_W ~ dnorm(0, 1),
+    b_T ~ dnorm(0, 1),
+    a_Var[Species] ~ dnorm(0 , sigma_Species),
+    sigma_Species ~ dcauchy(0, 1),
+    sigma ~ dexp(1)
+  ),
+  data = mod_list,
+  start = list(M_est = mod_list$M_obs,
+               Temp_est = mod_list$Temp_obs),
+  WAIC = FALSE,
+  iter = 3000,
+  warmup = 1500)
+
+## Run diagnostics
+
+check_energy(model_M_T_W@stanfit)
+check_treedepth(model_M_T_W@stanfit)
+
+divergent <- get_sampler_params(model_M_T_W@stanfit, inc_warmup=FALSE)[[1]][,'divergent__']
+sum(divergent)
+
+precis(model_M_T_W, digits = 4, prob = 0.95, depth = 2)
+
+## Save stanfit
+
+saveRDS(model_M_T_W, "Outputs/M_T_W/M_T_W_model.rds")
+
+## Extract samples
+
+## Plot intervals
+
+post <- extract.samples(model_M_T_W)
+post <- as.data.frame(post)
+
+colnames(post)[217:227] <- c("a", "b_W", "b_T", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "sigma_Species", "sigma")
+
+## Plot pairs
+
+pairs(model_M_T_W, pars = c("a", "b_W", "b_T", "sigma", "sigma_Species"))
+
+## Plot intervals
+
+color_scheme_set("darkgray")
+
+mcmc_intervals(post,
+               pars = c("a", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "b_W", "b_T",  "sigma_Species", "sigma"),
+               prob = 0.5, prob_outer = 0.95) +
+  labs(x = "Posterior Predictions", y = "Parameters") +
+  theme(panel.background = element_blank(),
+        legend.position = "none",
+        strip.background = element_rect(fill = "white"),
+        strip.text.x = element_text(size = 10, face = "italic"),
+        text = element_text(size = 10, family = "sans"),
+        panel.border = element_rect(colour = "black", fill = NA),
+        axis.text.x = element_text(colour = "black", face = "plain", size = 10),
+        axis.text.y = element_text(colour = "black", face = "plain", size = 10))
+
+## Plot trace
+
+p <- mcmc_trace(post, pars = c("a", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "b_W", "b_T",  "sigma_Species", "sigma"),
+                facet_args = list(nrow = 4, labeller = label_parsed))
+plot <- p + facet_text(size = 10) +
+  labs(x = "Number of Iterations", y = "Parameter Value") +
+  theme(panel.background = element_blank(), # Keep the background blank
+        text = element_text(size = 10, family = "sans"),
+        panel.border = element_rect(colour = "black", fill = NA),
+        axis.text = element_text(colour = "black", size = 10, face = "plain"))
+plot
+
--- Code/Temperature.R
+++ Code/Temperature.R
@@ -1,44 +1,115 @@
-#### Pseudo Bayesian Simulation of Temperature - All Data Points ####
+#### RJAGS Temperature ####
 
 library(tidyverse)
+library(rjags)
+library(coda)
 
-# Load data
+### Load in data
 
-myct <- read.csv("Data/Myctophids_Master.csv")
-myct <- filter(myct, d13C != "NA")
-d18O_sd <- sd(myct$D18O_vals)
-d18O_min <- min(myct$D18O_vals)
-d18O_max <- max(myct$D18O_vals)
+myct <- read.csv("Myctophids_Master.csv")
+myct$dif <- myct$d18O - myct$D18O_vals
 
-# Load function
+hist(rnorm(1000, 2, 3))
 
-load("Functions/PseudoBayes_Temp.Rdata")
+## Create temperature function
 
-# Run for all
+Temp <- function(Num){
+myct_1 <- dplyr::filter(myct, MyNumber == Num)
 
-Temp_values <- data.frame()
+## Try in JAGS
 
-for(i in 1:nrow(myct)){
-  t <- with(myct[i,],
-            PseudoBayes_Temp(d18O, 0.02, # Based on NOCS values
-                             D18O_vals, d18O_sd, # SD of values (as they span Scotia Sea depth and lat-long)
-                             d18O_min, d18O_max,
-                             10000))
-  Temp_values <- rbind(Temp_values, t)
+iso_list <- list(
+  iso = myct_1$dif,
+  sigma = 1/(0.33^2),
+  a_obs = 3.90,
+  a_var = 1/(0.24^2),
+  b_obs = -0.20,
+  b_var = 1/(0.019^2),
+  N = 1
+  #temp_mu = temp_mu,
+  #temp_sigma = 1/(temp_sigma^2)
+)
+
+inits <- list(Temp = 0.0)
+
+cat("model
+    {
+    for (i in 1:N){
+    mu[i] <- a_est + Temp * b_est
+    iso[i] ~ dnorm(mu[i], tau)
+    }
+    a_est ~ dnorm(a_obs, a_var)
+    b_est ~ dnorm(b_obs, b_var)
+    Temp ~ dnorm(2, 1/(3^2))
+    tau <- sigma
+    }", file="Temp_Jags.txt")
+
+jags_mod <- jags.model(file = "Temp_Jags.txt", data = iso_list, inits = inits, n.chains = 3, n.adapt = 50000)
+
+output <- coda.samples(jags_mod,
+                       c("Temp", "a_est", "b_est"),
+                       n.iter = 100000,
+                       thin = 50)
+
+## Summary and traceplots
+
+sum <- summary(output)
+print(sum)
+
+capture.output(c(sum), file = paste("Summary_", Num, ".txt", sep = ""))
+
+bmp(file = paste("Plot_Temp", Num, ".bmp", sep = ""))
+plot(output)
+dev.off()
+
+## Diagnostics
+
+gel <- gelman.diag(output, confidence = 0.95)
+gel <- gel$psrf
+gelman <- as.data.frame(gel[, 1])
+print(gelman)
+capture.output(gelman, file = "Gelmen_Temp.txt")
+
+samp_size <- as.data.frame(effectiveSize(output))
+print(samp_size)
+
+geweke <- coda::geweke.diag(output)
+geweke.all <- data.frame(matrix(NA, nrow = 3, ncol = 3))
+colstring <- rep(NA, 3)
+for (i in 1:3) {
+  geweke.tmp <- as.data.frame(geweke[[i]]$z)
+  geweke.all[, i] <- geweke.tmp
+  colstring[i] <- c(paste("chain", i, sep = ""))
+}
+rownames(geweke.all) <- coda::varnames(output)
+colnames(geweke.all) <- colstring
+geweke.all <- round(geweke.all, 3)
+w <- which(!is.nan(geweke[[1]]$z))
+geweke.all <- geweke.all[w, ]
+geweke_fail <- matrix(NA, nrow = 1, ncol = 3)
+for (i in 1:3) {
+  geweke_fail[1, i] <- sum(abs(geweke.all[, i]) > 1.96)
+}
+colnames(geweke_fail) <- paste("Chain", 1:3)
+rownames(geweke_fail) <- "Geweke"
+print(geweke_fail)
+
+capture.output(c(gelman, samp_size, geweke_fail), file = paste("Diagnostics_", Num, ".txt", sep = ""))
+
+## Posterior
+
+post_1 <- as.data.frame(output[[1]])
+post_2 <- as.data.frame(output[[2]])
+post_3 <- as.data.frame(output[[3]])
+
+post_full <- rbind(post_1, post_2, post_3)
+
+write.csv(post_full, paste("Post_Temp_", Num, ".csv", sep = ""))
 }
 
-myct_T <- cbind(myct, Temp_values)
+for(i in 1:nrow(myct)){
+  with(myct[i,],
+       Temp(MyNumber))
+}
 
-# Do a test plots to check nothing cray is happening
-
-plot(myct_T$temp) 
-with(myct_T,
-     plot(Temp, temp))
-with(myct_T,
-     boxplot(temp ~ sciname))
-min(myct_T$sd_temp)
-max(myct_T$sd_temp)
-min(myct_T$temp)
-max(myct_T$temp)
-
-write.csv(myct_T, "Outputs/Temperature.csv")
+Temp("BAS_220")
