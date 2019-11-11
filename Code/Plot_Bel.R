6302ab5d4f4fa8b30d428dd09e1e9cb0388ce5a5
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-11-11 14:57:11 +0000

    Tidied Belmaker graph

--- Code/Plot_Bel.R
+++ Code/Plot_Bel.R
@@ -13,15 +13,15 @@ cbPalette <- c("#56B4E9", "#0072B2", "#E69F00", "#D55E00", "#009E73", "#CC79A7")
   scale_fill_manual(values = cbPalette) +
   scale_colour_manual(values = cbPalette) +
   geom_errorbarh(aes(xmin = mean_Metabol - sd_Metabol, # Horizontal
-                     xmax = mean_Metabol + sd_Metabol), alpha = 0.2, lwd = 1) + # Colour error-bars according to species
+                     xmax = mean_Metabol + sd_Metabol, col = sciname), alpha = 0.3, lwd = 1) + # Colour error-bars according to species
   geom_errorbar(aes(ymin = mean_M - sd_M, # Vertical
-                    ymax = mean_M + sd_M), alpha = 0.2, lwd = 1) +
+                    ymax = mean_M + sd_M, col = sciname), alpha = 0.2, lwd = 1) +
   geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
-  geom_abline(intercept = 0.1859, slope = -0.0047, lwd = 1) +
+  #geom_abline(intercept = 0.1859, slope = -0.0047, lwd = 1) +
   # Customise the theme
-  scale_shape_manual(values = c(21, 22, 23, 24, 25, 24)) +
+  scale_shape_manual(values = c(21, 22, 23, 24, 25, 21)) +
   xlab("Mass-Specific Oxygen Consumption") +
-  ylab("M Values") +
+  ylab(expression("M" ["oto"])) +
   # Add error-bars using sd
   theme(panel.background = element_blank(), # Keep the background blank
         text = element_text(size = 15, family = "sans"),
