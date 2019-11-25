6368a153258fff70e571d7a579f8d5b99fa0dad8
Sarah Alewijnse <sra2g13@soton.ac.uk>
2019-11-25 11:54:52 +0000

    Added standard errors

--- Code/Summary_Master.R
+++ Code/Summary_Master.R
@@ -2,6 +2,10 @@
 
 library(tidyverse)
 
+## Create function for standard error
+
+se <- function(x) sd(x)/sqrt(length(x))
+
 ## M
 
 M <- read.csv("Outputs/M/Posteriors/M_Post.csv")
@@ -14,7 +18,11 @@ M_sd <- aggregate(M, by = list(M$MyNumber), FUN = sd)
 colnames(M_sd) <- c("MyNumber", "sd_M", "ds")
 M_sd <- select(M_sd, MyNumber, sd_M)
 
-M <- left_join(M_means, M_sd, by = "MyNumber")
+M_se <- aggregate(M, by = list(M$MyNumber), FUN = se)
+colnames(M_se) <- c("MyNumber", "se_M", "ds")
+M_se <- select(M_se, MyNumber, se_M)
+
+M <- left_join(M_means, M_sd, M_se, by = "MyNumber")
 
 ## Temp
 
@@ -28,12 +36,17 @@ Temp_sd <- aggregate(Temp, by = list(Temp$MyNumber), FUN = sd)
 colnames(Temp_sd) <- c("MyNumber", "sd_Temp", "ds")
 Temp_sd <- select(Temp_sd, MyNumber, sd_Temp)
 
-Temp <- left_join(Temp_means, Temp_sd, by = "MyNumber")
+Temp_se <- aggregate(Temp, by = list(Temp$MyNumber), FUN = se)
+colnames(Temp_se) <- c("MyNumber", "se_Temp", "ds")
+Temp_se <- select(Temp_se, MyNumber, se_Temp)
+
+Temp <- left_join(Temp_means, Temp_sd, Temp_se, by = "MyNumber")
 
 ## Belcher Metabolic Rate
 
 Bel <- read.csv("Outputs/Belcher/Posteriors/Belcher_Post.csv")
 
+# Change to oxygen consumption rather than log oxygen consumption
 e <- exp(1)
 Bel$Metabol_Convert <- e^Bel$Metabol
 
@@ -45,6 +58,10 @@ Bel_sd <- aggregate(Bel, by = list(Bel$MyNumber), FUN = sd)
 colnames(Bel_sd) <- c("MyNumber", "sd_log_Metabol", "ds", "sd_Metabol")
 Bel_sd <- select(Bel_sd, MyNumber, sd_Metabol, sd_log_Metabol)
 
+Bel_se <- aggregate(Bel, by = list(Bel$MyNumber), FUN = se)
+colnames(Bel_se) <- c("MyNumber", "se_log_Metabol", "ds", "se_Metabol")
+Bel_se <- select(Bel_se, MyNumber, se_Metabol, se_log_Metabol)
+
 Bel <- left_join(Bel_means, Bel_sd, by = "MyNumber")
 
 ## Combine
@@ -54,13 +71,5 @@ outputs <- left_join(Temp, M, by = "MyNumber")
 myct <- read.csv("Myctophids_Master.csv")
 
 all <- left_join(myct, outputs, by = "MyNumber")
-
-write.csv(all, "Myctophids_M_Temp.csv", row.names = FALSE)
-
-## With metabol
-
-myct <- read.csv("Myctophids_M_Temp.csv")
-
-all <- left_join(myct, Bel, by = "MyNumber")
 
 write.csv(all, "Myctophids_M_Temp_Bel.csv", row.names = FALSE)
