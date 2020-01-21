55b229422cc93b8f1a572bdb5e7b1d47c4df404e
Sarah Alewijnse <sra2g13@soton.ac.uk>
2020-01-21 10:35:05 -0600

    General update/I can't remmber what I was working on here

--- Code/LM_CI_Funcion.R
+++ Code/LM_CI_Funcion.R
@@ -12,6 +12,7 @@ lm.ci <- function(x_data,
   y0 <- a + x0*b
   y1 <- a + x1*b
   segments(x0, y0, x1, y1, lwd = 2)
+  if(x0 < 0){
   # Create sequence for x values
   x_seq_1 <- seq(from = min(x_data), to = 0, length.out = 10000)
   x_seq_2 <- seq(from = 0, to = max(x_data), length.out = 10000)
@@ -34,6 +35,31 @@ lm.ci <- function(x_data,
   yynew <- yy[order(Arg(scale(xx) + scale(yy) * 1i))]
   # Plot polygon
   polygon(xxnew, yynew, col = rgb(64, 64, 64, max = 255, alpha = 75), border = NA)
+  }
+  else {
+    x0 <- min(x_data)
+    x1 <- max(x_data)
+    y0 <- a + x0*b
+    y1 <- a + x1*b
+    segments(x0, y0, x1, y1, lwd = 2)
+    # Create sequence for x values
+    x_seq_1 <- seq(from = min(x_data), to = max(x_data), length.out = 10000)
+    # Create upper CI
+    up_ci <- a_up_ci + x_seq_1*b_up_ci
+    smooth_up_ci <- smooth.spline(up_ci, spar = 1)
+    # Create lower CI
+    low_ci <- a_low_ci + x_seq_1*b_low_ci
+    smooth_low_ci <- smooth.spline(low_ci, spar = 1)
+    # Create points for polygon
+    yy <- c(smooth_low_ci$y, smooth_up_ci$y)
+    xx <- c(x_seq_1, x_seq_1)
+    # Order points for polygon
+    xxnew <- xx[order(Arg(scale(xx) + scale(yy) * 1i))]
+    yynew <- yy[order(Arg(scale(xx) + scale(yy) * 1i))]
+    # Plot polygon
+    polygon(xxnew, yynew, col = rgb(64, 64, 64, max = 255, alpha = 75), border = NA)
+  }
 }
 
 save(lm.ci, file = "LM_CI.Rdata")
+
