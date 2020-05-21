e5fec9efec1cdc54ca5e89bf3369fb94a1035fad
Sarah Alewijnse <sra2g13@soton.ac.uk>
2020-05-21 16:12:00 +0100

    Tidied code and rearranged grids for body mass

--- Code/04_Plots/Plot_Ind.R
+++ Code/04_Plots/Plot_Ind.R
@@ -1,12 +1,14 @@
-### Plot Individual ###
+### Plot Intraspecific Body Mass ###
 
 library(tidyverse)
 library(gridExtra)
 
+## Get data
+
 myct <- read.csv("Data/Myctophids_M_Temp_Bel.csv")
-myct_tidy <- filter(myct, Weight_SD == "0")
-myct_tidy <- filter(myct_tidy, !is.na(mean_M))
-myct_tidy$ln_Weight <- log(myct_tidy$Weight.x)
+myct_tidy <- filter(myct, Weight_SD == "0") # Remove those without body mass
+myct_tidy <- filter(myct_tidy, !is.na(mean_M)) # Remove those without C_resp
+myct_tidy$ln_Weight <- log(myct_tidy$Weight.x) # Convert weight to log weight
 
 ## Subset to species
 
@@ -17,48 +19,62 @@ GYR <- filter(myct_tidy, Label == "GYR")
 GYN <- filter(myct_tidy, Label == "GYN")
 KRA <- filter(myct_tidy, Label == "KRA")
 
-## Plot mass
+#### ELN ####
 
-ELN_col <- c("#56B4E9")
+ELN_col <- c("#0072B2")
 
 ELN_plot <- ggplot(ELN, aes(ln_Weight, mean_M)) +
   scale_fill_manual(values = ELN_col) +
   scale_colour_manual(values = ELN_col) +
-  geom_errorbar(aes(ymin = mean_M - se_M, # Vertical
-                    ymax = mean_M + se_M, col = ELN_col), alpha = 0.3, lwd = 1) +
   geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
-  #geom_abline(aes(intercept = 0.1963, slope = 0.0262), lwd = 1) +
   # Customise the theme
   scale_shape_manual(values = c(21)) +
   xlab("ln(Body Mass) (g)") +
-  ylab(expression("M" ["oto"])) +
-  ggtitle("Electrona antarctica") +
-  # Add error-bars using sd
+  ylab(expression("C" ["resp"])) +
   theme(panel.background = element_blank(), # Keep the background blank
         text = element_text(size = 15),
         panel.border = element_rect(colour = "black", fill = NA),
         legend.position = "none",
-        plot.title = element_text(face = "italic", hjust = 0.5),
         axis.text.x = element_text(colour = "black"),
         axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
 
+ELN_plot <- ELN_plot + labs(tag = "A")
 ELN_plot
 
-ELC_col <- c("#0072B2")
+#### ELC ####
+
+ELC_col <- c("#56B4E9")
 
 ELC_plot <- ggplot(ELC, aes(ln_Weight, mean_M)) +
   scale_fill_manual(values = ELC_col) +
   scale_colour_manual(values = ELC_col) +
-  # Colour error-bars according to species
-  geom_errorbar(aes(ymin = mean_M - se_M, # Vertical
-                    ymax = mean_M + se_M, col = ELC_col), alpha = 0.3, lwd = 1) +
   geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
-  #geom_abline(aes(intercept = 0.1963, slope = 0.0262), lwd = 1) +
   # Customise the theme
   scale_shape_manual(values = c(22)) +
   xlab("ln(Body Mass) (g)") +
-  ylab(expression("M" ["oto"])) +
-  ggtitle("Electrona carlsbergi") +
+  ylab(expression("C" ["resp"])) +
+  theme(panel.background = element_blank(), # Keep the background blank
+        text = element_text(size = 15),
+        panel.border = element_rect(colour = "black", fill = NA),
+        legend.position = "none",
+        plot.title = element_text(face = "italic", hjust = 0.5),
+        axis.text.x = element_text(colour = "black"),
+        axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
+
+ELC_plot <- ELC_plot + labs(tag = "B")
+
+#### GYR ####
+
+GYR_col <- c("#E69F00")
+
+GYR_plot <- ggplot(GYR, aes(ln_Weight, mean_M)) +
+  scale_fill_manual(values = GYR_col) +
+  scale_colour_manual(values = GYR_col) +
+  geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
+  # Customise the theme
+  scale_shape_manual(values = c(23)) +
+  xlab("ln(Body Mass) (g)") +
+  ylab(expression("C" ["resp"])) +
   # Add error-bars using sd
   theme(panel.background = element_blank(), # Keep the background blank
         text = element_text(size = 15),
@@ -68,7 +84,51 @@ ELC_plot <- ggplot(ELC, aes(ln_Weight, mean_M)) +
         axis.text.x = element_text(colour = "black"),
         axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
 
-ELC_plot
+GYR_plot <- GYR_plot + labs(tag = "C")
+
+#### GYN ####
+
+GYN_col <- c("#D55E00")
+
+GYN_plot <- ggplot(GYN, aes(ln_Weight, mean_M)) +
+  scale_fill_manual(values = GYN_col) +
+  scale_colour_manual(values = GYN_col) +
+  geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
+  # Customise the theme
+  scale_shape_manual(values = c(24)) +
+  xlab("ln(Body Mass) (g)") +
+  ylab(expression("C" ["resp"])) +
+  # Add error-bars using sd
+  theme(panel.background = element_blank(), # Keep the background blank
+        text = element_text(size = 15),
+        panel.border = element_rect(colour = "black", fill = NA),
+        legend.position = "none",
+        axis.text.x = element_text(colour = "black"),
+        axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
+
+GYN_plot <- GYN_plot + labs(tag = "D")
+
+#### KRA ####
+
+KRA_col <- c("#009E73")
+
+KRA_plot <- ggplot(KRA, aes(ln_Weight, mean_M)) +
+  scale_fill_manual(values = KRA_col) +
+  scale_colour_manual(values = KRA_col) +
+  geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
+  # Customise the theme
+  scale_shape_manual(values = c(25)) +
+  xlab("ln(Body Mass) (g)") +
+  ylab(expression("C" ["resp"])) +
+  # Add error-bars using sd
+  theme(panel.background = element_blank(), # Keep the background blank
+        text = element_text(size = 15),
+        panel.border = element_rect(colour = "black", fill = NA),
+        legend.position = "none",
+        axis.text.x = element_text(colour = "black"),
+        axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
+
+KRA_plot <- KRA_plot + labs(tag = "E")
 
 #### PRM ####
 
@@ -82,12 +142,16 @@ for(i in 1:length(PRM$ln_Weight)){ # Loop to get z-scores
 
 PRM_col <- c("#CC79A7")
 
+# Get scaling relationship 
+
 load("LM_CI.Rdata")
 
 PRM_ribbon <- lm.ci(PRM$Weight_Z, a = 0.168,
                     b = 0.012, a_up_ci = 0.178,
                     a_low_ci = 0.158, b_up_ci = 0.022,
                     b_low_ci = 0.0012)
+
+# Set tick breaks
 
 z_1 <- (-0.5 - mean(PRM$ln_Weight)) / sd(PRM$ln_Weight)
 z_2 <- (0 - mean(PRM$ln_Weight)) / sd(PRM$ln_Weight)
@@ -98,19 +162,14 @@ PRM_plot <- ggplot(PRM, aes(Weight_Z, mean_M)) +
   scale_colour_manual(values = PRM_col) +
   scale_x_continuous(breaks = c(z_1, z_2, z_3),
                      labels = c(-0.5, 0, 0.5)) +
-  # Colour error-bars according to species
-  #geom_errorbar(aes(ymin = mean_M - se_M, # Vertical
-                    #ymax = mean_M + se_M, col = PRM_col), alpha = 0.3, lwd = 1) +
   geom_polygon(data = PRM_ribbon, aes(x_ribbon, y_ribbon), alpha = 0.3) +
   geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
-  #geom_abline(aes(intercept = 0.1676, slope = 0.0114), lwd = 1) +
   geom_segment(aes(x = min(Weight_Z), xend = max(Weight_Z), 
                    y = 0.168 + 0.012* min(Weight_Z), yend = 0.168 + 0.012* max(Weight_Z)), lwd = 1) +
   # Customise the theme
   scale_shape_manual(values = c(21)) +
   xlab("ln(Body Mass) (g)") +
   ylab(expression("C" ["resp"])) +
-  ggtitle("Protomyctophum bolini") +
   # Add error-bars using sd
   theme(panel.background = element_blank(), # Keep the background blank
         text = element_text(size = 15),
@@ -120,92 +179,7 @@ PRM_plot <- ggplot(PRM, aes(Weight_Z, mean_M)) +
         axis.text.x = element_text(colour = "black"),
         axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
 
-PRM_plot
-
-GYR_col <- c("#E69F00")
-
-GYR_ribbon <- lm.ci(GYR$ln_Weight, a = 0.2071,
-                    b = -0.0049, a_up_ci = 0.2139,
-                    a_low_ci = 0.2012, b_up_ci = -0.0127,
-                    b_low_ci = 0.0030)
-
-GYR_plot <- ggplot(GYR, aes(ln_Weight, mean_M)) +
-  scale_fill_manual(values = GYR_col) +
-  scale_colour_manual(values = GYR_col) +
-  # Colour error-bars according to species
-  geom_errorbar(aes(ymin = mean_M - se_M, # Vertical
-                    ymax = mean_M + se_M, col = GYR_col), alpha = 0.3, lwd = 1) +
-  geom_polygon(data = GYR_ribbon, aes(x_ribbon, y_ribbon), alpha = 0.3) +
-  geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
-  geom_segment(aes(x = min(ln_Weight), xend = max(ln_Weight), 
-                   y = 0.2071 + -0.0049* min(ln_Weight), yend = 0.2071 + -0.0049* max(ln_Weight)), lwd = 1) +
-  # Customise the theme
-  scale_shape_manual(values = c(23)) +
-  xlab("ln(Body Mass) (g)") +
-  ylab(expression("M" ["oto"])) +
-  ggtitle("Gymnoscopelus braueri") +
-  # Add error-bars using sd
-  theme(panel.background = element_blank(), # Keep the background blank
-        text = element_text(size = 15),
-        panel.border = element_rect(colour = "black", fill = NA),
-        legend.position = "none",
-        plot.title = element_text(face = "italic", hjust = 0.5),
-        axis.text.x = element_text(colour = "black"),
-        axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
-
-GYR_plot
-
-GYN_col <- c("#D55E00")
-
-GYN_plot <- ggplot(GYN, aes(ln_Weight, mean_M)) +
-  scale_fill_manual(values = GYN_col) +
-  scale_colour_manual(values = GYN_col) +
-  # Colour error-bars according to species
-  geom_errorbar(aes(ymin = mean_M - se_M, # Vertical
-                    ymax = mean_M + se_M, col = "GYN_col"), alpha = 0.3, lwd = 1) +
-  geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
-  #geom_abline(aes(intercept = 0.1963, slope = 0.0262), lwd = 1) +
-  # Customise the theme
-  scale_shape_manual(values = c(24)) +
-  xlab("ln(Body Mass) (g)") +
-  ylab(expression("M" ["oto"])) +
-  ggtitle("Gymnoscopelus nicholsi") +
-  # Add error-bars using sd
-  theme(panel.background = element_blank(), # Keep the background blank
-        text = element_text(size = 15),
-        panel.border = element_rect(colour = "black", fill = NA),
-        legend.position = "none",
-        plot.title = element_text (face = "italic", hjust = 0.5),
-        axis.text.x = element_text(colour = "black"),
-        axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
-
-GYN_plot
-
-KRA_col <- c("#009E73")
-
-KRA_plot <- ggplot(KRA, aes(ln_Weight, mean_M)) +
-  scale_fill_manual(values = KRA_col) +
-  scale_colour_manual(values = KRA_col) +
-  # Colour error-bars according to species
-  geom_errorbar(aes(ymin = mean_M - se_M, # Vertical
-                    ymax = mean_M + se_M, col = KRA_col), alpha = 0.3, lwd = 1) +
-  geom_point(aes(fill = sciname, shape = sciname), size = 4) + # Colour points according to species
-  #geom_abline(aes(intercept = 0.1963, slope = 0.0262), lwd = 1) +
-  # Customise the theme
-  scale_shape_manual(values = c(25)) +
-  xlab("ln(Body Mass) (g)") +
-  ylab(expression("M" ["oto"])) +
-  ggtitle("Krefftichtys anderssoni") +
-  # Add error-bars using sd
-  theme(panel.background = element_blank(), # Keep the background blank
-        text = element_text(size = 15),
-        panel.border = element_rect(colour = "black", fill = NA),
-        legend.position = "none",
-        plot.title = element_text(face = "italic", hjust = 0.5),
-        axis.text.x = element_text(colour = "black"),
-        axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
-
-KRA_plot
+PRM_plot <- PRM_plot + labs(tag = "F")
 
 grid.arrange(ELN_plot, ELC_plot,
              GYR_plot, GYN_plot,
