d601be694ab9e0e9c7ce5e9e688c3d63e3fdeb6e
Sarah Alewijnse <sra2g13@soton.ac.uk>
2020-05-21 11:44:13 +0100

    Updated to C_resp and tidied

--- Code/04_Plots/Plots_Body_Mass_Temp.R
+++ Code/04_Plots/Plots_Body_Mass_Temp.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(gridExtra)
 library(rethinking)
 
-myct <- read.csv("Myctophids_M_Temp_Bel.csv")
+myct <- read.csv("Data/Myctophids_M_Temp_Bel.csv")
 myct_tidy <- filter(myct, Weight_SD == "0")
 myct_tidy <- filter(myct_tidy, !is.na(mean_M))
 myct_tidy$ln_Weight <- log(myct_tidy$Weight.x)
@@ -27,7 +27,7 @@ plot1 <- ggplot(myct, aes(mean_Temp, mean_M, sciname)) +
   # Customise the theme
   scale_shape_manual(values = c(21, 22, 23, 24, 25, 21)) +
   xlab(expression('Temperature ('*~degree*C*')')) +
-  ylab(expression("M" ["oto"])) +
+  ylab(expression("C" ["resp"])) +
   # Add error-bars using sd
   theme(panel.background = element_blank(), # Keep the background blank
         text = element_text(size = 15),
@@ -39,30 +39,6 @@ plot1 <- ggplot(myct, aes(mean_Temp, mean_M, sciname)) +
         axis.text.y = element_text(colour = "black"))  # Print the minor gridlines
 
 plot1
-
-# Do in base
-
-colours <- cbPalette[as.numeric(myct_tidy$sciname)]
-
-shapes <- c(21, 22, 23, 24, 25, 21)
-shapes <- shapes[as.numeric(myct_tidy$sciname)]
-
-plot(mean_M ~ mean_Temp, data = myct_tidy, col = "black", bg = colours, pch = shapes)
-  with(myct_tidy,
-       arrows(x0 = mean_Temp, y0 = mean_M - se_M,
-              x1 = mean_Temp, y1 = mean_M + se_M, code = 0, col = colours))
-  with(myct_tidy,
-       arrows(x0 = mean_Temp - se_Temp, y0 = mean_M,
-              x1 = mean_Temp + se_Temp, y1 = mean_M, code = 0, col = colours))
-  
-model_M_T_W <- readRDS("Outputs/M_T_W/M_T_W_model.rds")
-post <- extract.samples(model_M_T_W)
-mu <- link(model_M_T_W, data = mod_list)
-mu.mean <- apply(mu, 2, mean)
-mu.PI <- apply(mu, 2, PI, prob = 0.95)
-temp_seq <- seq(from = min(myct_tidy$mean_Temp), to = max(myct_tidy$mean_Temp, length.out = length(post)))
-lines(temp_seq, mu.mean)
-
 
 ## Body mass
 
@@ -76,7 +52,7 @@ plot2 <- ggplot(myct_tidy, aes(ln_Weight, mean_M, sciname)) +
   # Customise the theme
   scale_shape_manual(values = c(21, 22, 23, 24, 25, 21)) +
   xlab("ln(Body Mass) (g)") +
-  ylab(expression("M" ["oto"])) +
+  ylab(expression("C" ["resp"])) +
   # Add error-bars using sd
   theme(panel.background = element_blank(), # Keep the background blank
         text = element_text(size = 15),
