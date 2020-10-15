a53d107971da7dac1983f6e3a3ef35e7a2483164
Sarah Alewijnse <sra2g13@soton.ac.uk>
2020-10-15 12:02:22 +0100

    Reran and updated to automate

--- Code/02_Linear_Models_Among_Species/03_M_Temperature.R
+++ Code/02_Linear_Models_Among_Species/03_M_Temperature.R
@@ -6,22 +6,19 @@ library(bayesplot)
 
 options(max.print=999999)
 
-myct <- read.csv("Myctophids_M_Temp.csv")
+myct <- read.csv("Data/Myctophids_M_Temp_Bel.csv")
 glimpse(myct)
 
-#### Overall Model with Weight and Temp ####
+#### Overall Model with Temperature ####
 
-myct_tidy <- filter(myct, Weight_SD == "0")
 myct_tidy <- filter(myct, !is.na(mean_M))
-myct_tidy$log_Weight <- log(myct_tidy$Weight.x)
 glimpse(myct_tidy)
 
-M_T_W_list <- list(
+M_T_list <- list(
   M_obs = myct_tidy$mean_M,
-  M_sd = myct_tidy$sd_M,
-  Weight = myct_tidy$log_Weight,
+  M_se = myct_tidy$se_M,
   Temp_obs = myct_tidy$mean_Temp,
-  Temp_sd = myct_tidy$sd_Temp,
+  Temp_se = myct_tidy$se_Temp,
   Species = myct_tidy$sciname
 )
 
@@ -29,31 +26,21 @@ M_T_W_list <- list(
 
 # Temp_obs
 
-Temp_obs_mean <- mean(M_T_W_list$Temp_obs)
-Temp_obs_sd <- sd(M_T_W_list$Temp_obs)
+Temp_obs_mean <- mean(M_T_list$Temp_obs)
+Temp_obs_sd <- sd(M_T_list$Temp_obs)
 
-for(i in 1:length(M_T_W_list$Temp_obs)){
-  M_T_W_list$Temp_Obs_Z[i] <- (M_T_W_list$Temp_obs[i] - Temp_obs_mean) / Temp_obs_sd
-}
-
-# Temp_sd
-
-Temp_sd_mean <- mean(M_T_W_list$Temp_sd)
-Temp_sd_sd <- sd(M_T_W_list$Temp_sd)
-
-for(i in 1:length(M_T_W_list$Temp_sd)){
-  M_T_W_list$Temp_SD_Z[i] <- abs((M_T_W_list$Temp_sd[i] - Temp_sd_mean) / Temp_sd_sd)
+for(i in 1:length(M_T_list$Temp_obs)){
+  M_T_list$Temp_Obs_Z[i] <- (M_T_list$Temp_obs[i] - Temp_obs_mean) / Temp_obs_sd
 }
 
 # Tidy list
 
 mod_list <- list(
-  M_obs = M_T_W_list$M_obs,
-  M_sd = M_T_W_list$M_sd,
-  Weight = M_T_W_list$Weight_Z,
-  Temp_obs = M_T_W_list$Temp_Obs_Z,
-  Temp_sd = M_T_W_list$Temp_SD_Z,
-  Species = M_T_W_list$Species
+  M_obs = M_T_list$M_obs,
+  M_se = M_T_list$M_se,
+  Temp_obs = M_T_list$Temp_Obs_Z,
+  Temp_se = M_T_list$Temp_se,
+  Species = M_T_list$Species
 )
 
 ## Model
@@ -67,8 +54,8 @@ model_M_T <- map2stan(
       b_T*Temp_est[i],
     
     # Data uncertainties
-    M_obs ~ dnorm(M_est, M_sd),
-    Temp_obs ~ dnorm(Temp_est, Temp_sd),
+    M_obs ~ dnorm(M_est, M_se),
+    Temp_obs ~ dnorm(Temp_est, Temp_se),
     
     # Parameters
     a ~ dnorm(0, 1),
@@ -96,46 +83,6 @@ precis(model_M_T, digits = 4, prob = 0.95, depth = 2)
 
 ## Save stanfit
 
-saveRDS(model_M_T, "Outputs/M_T/M_T_model.rds")
+saveRDS(model_M_T, "Outputs/02_Linear_Models_Among_Species/03_M_Temperature/M_T_model.rds")
 
-## Extract samples
 
-## Plot intervals
-
-post <- extract.samples(model_M_T)
-post <- as.data.frame(post)
-
-colnames(post)[217:226] <- c("a", "b_T", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "sigma_Species", "sigma")
-
-## Plot pairs
-
-pairs(model_M_T, pars = c("a", "b_T", "sigma", "sigma_Species"))
-
-## Plot intervals
-
-color_scheme_set("darkgray")
-
-mcmc_intervals(post,
-               pars = c("a", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "b_T",  "sigma_Species", "sigma"),
-               prob = 0.5, prob_outer = 0.95) +
-  labs(x = "Posterior Predictions", y = "Parameters") +
-  theme(panel.background = element_blank(),
-        legend.position = "none",
-        strip.background = element_rect(fill = "white"),
-        strip.text.x = element_text(size = 10, face = "italic"),
-        text = element_text(size = 10, family = "sans"),
-        panel.border = element_rect(colour = "black", fill = NA),
-        axis.text.x = element_text(colour = "black", face = "plain", size = 10),
-        axis.text.y = element_text(colour = "black", face = "plain", size = 10))
-
-## Plot trace
-
-p <- mcmc_trace(post, pars = c("a", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "b_T",  "sigma_Species", "sigma"),
-                facet_args = list(nrow = 4, labeller = label_parsed))
-plot <- p + facet_text(size = 10) +
-  labs(x = "Number of Iterations", y = "Parameter Value") +
-  theme(panel.background = element_blank(), # Keep the background blank
-        text = element_text(size = 10, family = "sans"),
-        panel.border = element_rect(colour = "black", fill = NA),
-        axis.text = element_text(colour = "black", size = 10, face = "plain"))
-plot
---
+++ Code/02_Linear_Models_Among_Species/03b_M_Temperature.R
@@ -0,0 +1,101 @@
+#### Body Mass Model Outputs ####
+
+library(tidyverse)
+library(rethinking)
+library(bayesplot)
+
+options(max.print=999999)
+
+#### Table Output ####
+
+model_M_T <- readRDS("Outputs/02_Linear_Models_Among_Species/03_M_Temperature/M_T_model.rds")
+
+## Get Precis table
+
+table <- precis(model_M_T, digits = 4, prob = 0.95, depth = 2)
+table
+
+## Get outputs
+
+means <- data.frame()
+for(i in 217:226){
+  m <- table[i, 1]
+  means <- rbind(means, m)
+}
+
+sds <- data.frame()
+for(i in 217:226){
+  m <- table[i, 2]
+  sds <- rbind(sds, m)
+}
+
+## Add variable names
+
+precis_tidy <- cbind(means, sds)
+var_names <- c("a", "b_T", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "sigma_Species", "sigma")
+precis_tidy <- cbind(var_names, precis_tidy)
+colnames(precis_tidy) <- c("var_names", "mean", "stan_dev")
+
+## Get calculations
+
+adj_means <- data.frame()
+for(i in 3:8){
+  a_mean <- precis_tidy[1,2]
+  sp_var_mean <- precis_tidy[i, 2]
+  adj <- a_mean + sp_var_mean
+  adj_means <- rbind(adj_means, adj)
+}
+
+precis_adj <- precis_tidy
+precis_adj[c(3:8), 2] <- adj_means
+precis_adj[, 2:3] <- round(precis_adj[,2:3], digits = 3)
+
+precis_adj ### Use this for results
+
+#### Graph Output ####
+
+## Extract samples
+
+post <- extract.samples(model_M_T)
+post <- as.data.frame(post)
+
+colnames(post)[217:226] <- c("a", "b_T", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "sigma_Species", "sigma")
+
+## Plot pairs
+
+svg(file = "Outputs/02_Linear_Models_Among_Species/03_M_Temperature/Pairs.svg")
+pairs(model_M_T, pars = c("a", "b_T", "sigma", "sigma_Species"))
+dev.off()
+
+## Plot intervals
+
+color_scheme_set("darkgray")
+
+svg(file = "Outputs/02_Linear_Models_Among_Species/03_M_Temperature/Posterior.svg")
+mcmc_intervals(post,
+               pars = c("a", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "b_T",  "sigma_Species", "sigma"),
+               prob = 0.5, prob_outer = 0.95) +
+  labs(x = "Posterior Predictions", y = "Parameters") +
+  theme(panel.background = element_blank(),
+        legend.position = "none",
+        strip.background = element_rect(fill = "white"),
+        strip.text.x = element_text(size = 10, face = "italic"),
+        text = element_text(size = 10, family = "sans"),
+        panel.border = element_rect(colour = "black", fill = NA),
+        axis.text.x = element_text(colour = "black", face = "plain", size = 10),
+        axis.text.y = element_text(colour = "black", face = "plain", size = 10))
+dev.off()
+
+## Plot trace
+
+svg(file = "Outputs/02_Linear_Models_Among_Species/03_M_Temperature/Traceplot.svg")
+p <- mcmc_trace(post, pars = c("a", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "b_T",  "sigma_Species", "sigma"),
+                facet_args = list(nrow = 4, labeller = label_parsed))
+plot <- p + facet_text(size = 10) +
+  labs(x = "Number of Iterations", y = "Parameter Value") +
+  theme(panel.background = element_blank(), # Keep the background blank
+        text = element_text(size = 10, family = "sans"),
+        panel.border = element_rect(colour = "black", fill = NA),
+        axis.text = element_text(colour = "black", size = 10, face = "plain"))
+plot
+dev.off()
