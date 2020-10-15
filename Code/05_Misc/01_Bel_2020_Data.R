7139d143d2f89088601e8036c19a32f2c7312731
Sarah Alewijnse <sra2g13@soton.ac.uk>
2020-10-15 17:26:50 +0100

    Redid and automated graph saving

--- Code/05_Misc/01_Bel_2020_Data.R
+++ Code/05_Misc/01_Bel_2020_Data.R
@@ -3,24 +3,19 @@
 library(tidyverse)
 library(readr)
 
-Bel_2020 <- read.csv("Data/Belcher_2020_Data/Belcher_SupplementaryData_COMICS.csv")
+Bel_2020 <- read.csv("Archive/Belcher_2020_Data/Belcher_SupplementaryData_COMICS.csv")
 
-# Conver from ul/mg/d to mg/kg/h
+#### Convert from ul/mg/d to mg/kg/h ####
 
 ul_mg <- function(ul){
   b <- ul * 1e6
-  mg <- (b * 0.001309) / 24 # Density of oxygen (mg/m^3) at 1 bar and 21 C
+  mg <- (b * 0.0014) / 24 # Density of oxygen (mg/m^3) at 1 bar and 21 C
   print(mg)
 }
 
 Bel_2020$mg_kg_h <- ul_mg(Bel_2020$R_WM..µL.O2..mg.WW.d.)
 
-# Get means and SDs
-
-mg_kg_means <- aggregate(Bel_2020, by = list(Bel_2020$Species), FUN = mean)
-mg_kg_sd <- aggregate(Bel_2020, by = list(Bel_2020$Species), FUN = sd)
-
-# Convert to C_resp
+#### Convert to C_resp ####
 
 C_resp <- function(mg_kg){
   C <- 27.46 # Upper bound - Martino et al. 2020
@@ -29,6 +24,21 @@ C_resp <- function(mg_kg){
   print(C_resp)
 }
 
-C_resp(629.37)
-C_resp(177.76)
-C_resp(76.83)
+Bel_2020$C_resp <- C_resp(Bel_2020$mg_kg_h)
+
+# Get means and SDs
+
+mg_kg_means <- aggregate(Bel_2020, by = list(Bel_2020$Species), FUN = mean)
+mg_kg_sd <- aggregate(Bel_2020, by = list(Bel_2020$Species), FUN = sd)
+
+# Tidy
+
+oxygen_consumption <- data.frame(Taxa = mg_kg_means$Group.1,
+                                 mg_kg_means = mg_kg_means$mg_kg_h,
+                                 mg_kg_sd = mg_kg_sd$mg_kg_h,
+                                 C_resp_mean = mg_kg_means$C_resp,
+                                 C_resp_sd = mg_kg_sd$C_resp)
+
+oxygen_consumption[,c(2:5)] <- round(oxygen_consumption[,c(2:5)], digits = 2)
+
+write.csv(oxygen_consumption, "Outputs/04_Misc/03_Bel_2020_Data_Conversion/Bel_2020_Conversion.csv")
--- Code/05_Misc/02_Bayes_Crushed.R
+++ Code/05_Misc/02_Bayes_Crushed.R
@@ -6,7 +6,7 @@ library(bayesplot)
 
 options(max.print=999999)
 
-myct <- read.csv("Data/Myctophids_M_Temp.csv")
+myct <- read.csv("Data/Myctophids_M_Temp_Bel.csv")
 glimpse(myct)
 
 #### Within PRM ####
@@ -16,7 +16,7 @@ glimpse(PRM_tidy)
 
 mod_list <- list(
   M_obs = PRM_tidy$mean_M,
-  M_sd = PRM_tidy$sd_M,
+  M_se = PRM_tidy$se_M,
   Crushed = PRM_tidy$Crushed
 )
 
@@ -30,7 +30,7 @@ model_PRM_crush <- map2stan(
     mu <- a + b_C*Crushed,
     
     # Data uncertainties
-    M_obs ~ dnorm(M_est, M_sd),
+    M_obs ~ dnorm(M_est, M_se),
     
     # Parameters
     a ~ dnorm(0, 1),
@@ -56,48 +56,7 @@ precis(model_PRM_crush, digits = 4, prob = 0.95, depth = 2)
 
 # Save model
 
-saveRDS(model_PRM_crush, "Outputs/05_Misc/Supplementary_Crush_Mill/PRM_Crush_model.rds")
-
-# Extract samples
-
-post <- extract.samples(model_PRM_crush)
-post <- as.data.frame(post)
-
-colnames(post)[21:23] <- c("a", "b_C", "sigma")
-
-## Plot pairs
-
-pairs(model_PRM_crush, pars = c("a", "b_C", "sigma"))
-
-## Plot intervals
-
-color_scheme_set("darkgray")
-
-mcmc_intervals(post,
-               pars = c("sigma", "b_C", "a"),
-               prob = 0.5, prob_outer = 0.95) +
-  labs(x = "Posterior Predictions", y = "Parameters") +
-  scale_y_discrete(labels = c("sigma", expression("b" ["Prep"]), "a")) +
-  theme(panel.background = element_blank(),
-        legend.position = "none",
-        strip.background = element_rect(fill = "white"),
-        strip.text.x = element_text(size = 15, face = "italic"),
-        text = element_text(size = 15, family = "sans"),
-        panel.border = element_rect(colour = "black", fill = NA),
-        axis.text.x = element_text(colour = "black", face = "plain", size = 15),
-        axis.text.y = element_text(colour = "black", face = "plain", size = 15))
-
-## Plot trace
-
-p <- mcmc_trace(post, pars = c("a", "b_C", "sigma"),
-                facet_args = list(nrow = 4, labeller = label_parsed))
-plot <- p + facet_text(size = 10) +
-  labs(x = "Number of Iterations", y = "Parameter Value") +
-  theme(panel.background = element_blank(), # Keep the background blank
-        text = element_text(size = 10, family = "sans"),
-        panel.border = element_rect(colour = "black", fill = NA),
-        axis.text = element_text(colour = "black", size = 10, face = "plain"))
-plot
+saveRDS(model_PRM_crush, "Outputs/04_Misc/02_Supplementary_Crush_Mill/PRM_Crush_model.rds")
 
 #### All with Species ####
 
@@ -105,10 +64,12 @@ myct_tidy <- filter(myct, !is.na(mean_M))
 
 mod_list <- list(
   M_obs = myct_tidy$mean_M,
-  M_sd = myct_tidy$sd_M,
+  M_se = myct_tidy$se_M,
   Crushed = myct_tidy$Crushed,
   Species = myct_tidy$sciname
 )
+
+mod_tab <- as.data.frame(mod_list)
 
 # Model
 
@@ -120,7 +81,7 @@ model_all_crush <- map2stan(
     mu <- a + a_Var[Species] + b_C*Crushed,
     
     # Data uncertainties
-    M_obs ~ dnorm(M_est, M_sd),
+    M_obs ~ dnorm(M_est, M_se),
     
     # Parameters
     a ~ dnorm(0, 1),
@@ -148,45 +109,6 @@ precis(model_all_crush, digits = 4, prob = 0.95, depth = 2)
 
 # Save model
 
-saveRDS(model_all_crush, "Outputs/05_Misc/Supplementary_Crush_Mill/All_Crush_model.rds")
+saveRDS(model_all_crush, "Outputs/04_Misc/02_Supplementary_Crush_Mill/All_Crush_model.rds")
 
-# Extract samples
 
-post <- extract.samples(model_all_crush)
-post <- as.data.frame(post)
-
-colnames(post)[109:118] <- c("a", "b_C", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "sigma_Species", "sigma")
-
-## Plot pairs
-
-pairs(model_all_crush, pars = c("a", "b_C", "sigma_Species", "sigma"))
-
-## Plot intervals
-
-color_scheme_set("darkgray")
-
-mcmc_intervals(post,
-               pars = c("sigma", "sigma_Species", "a_Var_GYN", "a_Var_PRM", "a_Var_ELC", "a_Var_KRA", "a_Var_GYR",  "a_Var_ELN", "b_C", "a"),
-               prob = 0.5, prob_outer = 0.95) +
-  labs(x = "Posterior Predictions", y = "Parameters") +
-  scale_y_discrete(labels = c("sigma", expression("sigma" ["species"]), expression("a_Var" ["GYN"]), expression("a_Var" ["PRM"]), expression("a_Var" ["ELC"]), expression("a_Var" ["KRA"]), expression("a_Var" ["GYR"]), expression("a_Var" ["ELN"]), expression ("b" ["C"]), "a")) +
-  theme(panel.background = element_blank(),
-        legend.position = "none",
-        strip.background = element_rect(fill = "white"),
-        strip.text.x = element_text(size = 15, face = "italic"),
-        text = element_text(size = 15, family = "sans"),
-        panel.border = element_rect(colour = "black", fill = NA),
-        axis.text.x = element_text(colour = "black", face = "plain", size = 15),
-        axis.text.y = element_text(colour = "black", face = "plain", size = 15))
-
-## Plot trace
-
-p <- mcmc_trace(post, pars = c("a", "b_C", "a_Var_ELN", "a_Var_ELC", "a_Var_GYR", "a_Var_GYN", "a_Var_KRA", "a_Var_PRM", "sigma_Species", "sigma"),
-                facet_args = list(nrow = 4, labeller = label_parsed))
-plot <- p + facet_text(size = 10) +
-  labs(x = "Number of Iterations", y = "Parameter Value") +
-  theme(panel.background = element_blank(), # Keep the background blank
-        text = element_text(size = 10, family = "sans"),
-        panel.border = element_rect(colour = "black", fill = NA),
-        axis.text = element_text(colour = "black", size = 10, face = "plain"))
-plot
